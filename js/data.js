5372134b59f523e1fc2cb3a7856c35371ba6beab
shinjan <shinjanxp@gmail.com>
2014-06-26 01:40:29 +0530

    parallax

---
+++ js/data.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+ $(document).ready(function() {
+	 
+var front_height=$('#front').height();
+$('#hello').data("0","position:absolute; top:"+front_height+"px");
+alert("executed");
+
+});
